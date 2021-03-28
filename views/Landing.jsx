ade1f644ffa3d40afeef4d11f78374c5ca775a39
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-28 17:42:49 -0500

    Refactored Landing page

--- views/Landing.jsx
+++ views/Landing.jsx
@@ -1,13 +1,27 @@
 const React = require('react')
 const Layout = require('./Layout.jsx')
 
-const Landing = (props) => {
-	const greeting = props.hello;
-	return (
-		<Layout title="Welcome Page">
-			<h1 className="indexHeader">Hello User, please sign up or login.</h1>
-		</Layout>
-	);
-};
+class Landing extends React.Component{
+	render() {
+		return (
+			<Layout title='Digital Shelfie'>
+				<h1 className='indexHeader'>Hello User. Please sign up or login.</h1>
+			</Layout>
+		)
+	}
+}
 
 module.exports = Landing
+
+
+
+// const Landing = (props) => {
+// 	const greeting = props.hello;
+// 	return (
+// 		<Layout title="Welcome Page">
+// 			<h1 className="indexHeader">Hello User, please sign up or login.</h1>
+// 		</Layout>
+// 	);
+// };
+
+// module.exports = Landing
