da3e18e98c6ac54b12efa6ab45b57fa1a9680ff7
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-29 20:34:40 -0500

    Added the files that I had previously deleted

---
+++ index.js
@@ -0,0 +1,7 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const App = require('./App.js')
+
+ReactDOM.render(App, document.getElementById("root"));
+
+// This page is working fine, don't touch it
