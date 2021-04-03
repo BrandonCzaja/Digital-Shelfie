cea16b2dd1a5ce3a944e2008b64fafdfe479ffdc
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-04-02 22:37:56 -0500

    Adjusted the layout and styling of the login and sign up pages

--- views/auth/signup.jsx
+++ views/auth/signup.jsx
@@ -5,6 +5,7 @@ class Index extends React.Component {
   render() {
     return (
       <Layout title="SIGNUP">
+        <h2>Please sign up</h2>
         <form action="/auth/signup" method="post">
           <input type="text" name="username" placeholder="username" />
           <input type="password" name="password" placeholder="password" />
