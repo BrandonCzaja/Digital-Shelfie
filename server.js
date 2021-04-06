2e04a16c18652098f5b7df210eb19c6fbd913110
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-04-05 20:45:36 -0500

    Brought back my server.js content

--- server.js
+++ server.js
@@ -1,61 +1,61 @@
 ///////////////////////////
 // Environmental Variables
 ///////////////////////////
-// require("dotenv").config();
-// const { PORT, SECRET } = process.env;
+require("dotenv").config();
+const { PORT, SECRET } = process.env;
 
-// ///////////////////////////
-// // Dependencies
-// //////////////////////////
+///////////////////////////
+// Dependencies
+//////////////////////////
 
-// // Bringing in Express - This is the correct way, don't use import
+// Bringing in Express - This is the correct way, don't use import
 const express = require("express");
 const app = express();
 
-// // IMPORT DATABASE CONNECTIONS
-// const mongoose = require("./db/dbconn");
+// IMPORT DATABASE CONNECTIONS
+const mongoose = require("./db/dbconn");
 
-// // OTHER IMPORTS
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo")(session);
-// const methodOverride = require("method-override");
-// const morgan = require("morgan");
+// OTHER IMPORTS
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 
-// //set view engine to express-react-views
-// app.set("view engine", "jsx");
-// app.engine("jsx", require("express-react-views").createEngine());
-
-
-// // SESSIONS, this allows you to use req.session for tracking session data
-// app.use(
-// 	session({
-// 		secret: SECRET,
-// 		saveUninitialized: false, // don't create session until something stored
-// 		resave: false, //don't save session if unmodified
-// 		store: new MongoStore({ mongooseConnection: mongoose.connection }),
-// 	})
-// );
-// app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: true })); //comment if not using forms
-// app.use(methodOverride("_method"));
-// app.use(express.json()) 
-// app.use(morgan("tiny")); //logging
-
-// ///////////////
-// //Routes and Routers
-// //////////////
+//set view engine to express-react-views
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
 
-// // ROUTERS
-// const authRouter = require("./controllers/auth");
-// // const testRouter = require("./controllers/test");
-// const collectionRouter = require("./controllers/collection");
+// SESSIONS, this allows you to use req.session for tracking session data
+app.use(
+	session({
+		secret: SECRET,
+		saveUninitialized: false, // don't create session until something stored
+		resave: false, //don't save session if unmodified
+		store: new MongoStore({ mongooseConnection: mongoose.connection }),
+	})
+);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); //comment if not using forms
+app.use(methodOverride("_method"));
+app.use(express.json()) 
+app.use(morgan("tiny")); //logging
+
+///////////////
+//Routes and Routers
+//////////////
 
 
-// // Must have for auth to work
-// app.use("/auth", authRouter);
-// // Required for collection routes
-// app.use("/collection", collectionRouter);
+// ROUTERS
+const authRouter = require("./controllers/auth");
+// const testRouter = require("./controllers/test");
+const collectionRouter = require("./controllers/collection");
+
+
+// Must have for auth to work
+app.use("/auth", authRouter);
+// Required for collection routes
+app.use("/collection", collectionRouter);
 
 
 // Leave this at auth/login, otherwise heroku doesn't like it
